b669358ca9f4300cb9e774a865e4105adcab786b
Gayatri Loungani <97401692+Gayatriloungani@users.noreply.github.com>
2024-07-03 12:28:20 +0530

    Add files via upload

---
+++ backend/routes/paymentRoute.js
@@ -0,0 +1,10 @@
+// import express from "express";
+// import { orderController, verifyController } from "../controllers/paymentController.js";
+
+
+// const router = express.Router();
+
+// router.post("/orders" , orderController );
+// router.post("/verify" , verifyController);
+
+// export default router;
